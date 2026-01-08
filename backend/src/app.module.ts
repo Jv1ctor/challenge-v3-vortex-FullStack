965fa1515b5a4c2402081bc4f1762f5a1639c6ae
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-01-08 18:10:09 -0300

    fix(app): add db module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { RouterModule } from '@nestjs/core';
 import { UsersModule } from './users/users.module';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    RouterModule.register([
-      { path: "api" }
-    ]),
+    DbModule,
     UsersModule,
     AuthModule
-  ],
-  controllers: [AuthController],
+  ]
 })
 export class AppModule {}
