868d8b848b578fc56d8b1a02c18cc5efef94940f
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-01-09 08:32:31 -0300

    feat(entity): add factory entity and adjust relations with users

---
+++ backend/src/factories/entities/factory.entity.ts
@@ -0,0 +1,23 @@
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('factories')
+export class Factory {
+  @PrimaryGeneratedColumn('identity')
+  id: number;
+
+  @Column('varchar', { nullable: true, length: 225 })
+  name: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @OneToMany(() => User, (user) => user.factory)
+  users: User[];
+}
--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,20 +1,31 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
-
+import { Factory } from 'src/factories/entities/factory.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('users')
 export class User {
-  @PrimaryGeneratedColumn("uuid")
-  id: string 
-  
-  @Column("varchar", { length: 200, nullable: true, unique: true })
-  name: string
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-  @Column("varchar",  { nullable: true })
-  password: string
+  @Column('varchar', { length: 200, nullable: true, unique: true })
+  name: string;
 
-  @Column("bool", { name: "is_admin", default: false })
-  isAdmin: boolean
+  @Column('varchar', { nullable: true })
+  password: string;
 
-  @CreateDateColumn({ name: "created_at" })
-  createdAt: Date
+  @Column('bool', { name: 'is_admin', default: false })
+  isAdmin: boolean;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @ManyToOne(() => Factory, (factory) => factory.users)
+  @JoinColumn({ name: 'factory_id' })
+  factory: Factory;
 }
