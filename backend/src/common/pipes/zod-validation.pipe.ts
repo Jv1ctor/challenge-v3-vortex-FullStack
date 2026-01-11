8ed79291eb3763f7979baa397b07f9ae276c761d
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-01-11 13:33:16 -0300

    feat(zod-pipe): add new message for validations erros

--- backend/src/common/pipes/zod-validation.pipe.ts
+++ backend/src/common/pipes/zod-validation.pipe.ts
@@ -1,16 +1,27 @@
 import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
-import z from 'zod';
+import z, { ZodError } from 'zod';
 
 @Injectable()
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: z.ZodObject) {}
+
+  private getZodMessageError(zodError: ZodError): string[] {
+    return zodError.issues.map((issue) => issue.message);
+  }
 
   transform(value: unknown) {
     try {
       const parsedValue = this.schema.parse(value);
       return parsedValue;
     } catch (error) {
-      throw new BadRequestException('Validation failed', { cause: error });
+      let messageError: string[] = [];
+      if (error instanceof ZodError)
+        messageError = this.getZodMessageError(error);
+
+      throw new BadRequestException(messageError, {
+        cause: error,
+        description: 'Validation failed',
+      });
     }
   }
 }
