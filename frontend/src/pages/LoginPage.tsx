0c3e5e6223089972764d90d568fa3eeebc70aec7
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-01-13 17:07:02 -0300

    feat(login): add form background and modifiers components

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -15,9 +15,9 @@ import {
   CardHeader,
   CardTitle,
 } from "../components/ui/card"
-import { EyeIcon, EyeOffIcon } from "lucide-react"
+import { EyeIcon, EyeOffIcon, User } from "lucide-react"
 import { useState } from "react"
-
+import { Separator } from "@/components/ui/separator"
 function LoginPage() {
   const [name, setName] = useState("")
   const [password, setPassword] = useState("")
@@ -29,56 +29,90 @@ function LoginPage() {
 
   return (
     <>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Card className="w-full max-w-sm">
-          <CardHeader>
-            <CardTitle>Login</CardTitle>
-            <CardDescription>
-              Enter your user name below to login to your account
-            </CardDescription>
-          </CardHeader>
+      <div className="grid grid-cols-2">
+        <div>
+          <div className="flex min-h-svh items-center justify-center">
+            <Card className="w-full max-w-md border-0 shadow-none">
+              <CardHeader>
+                <CardTitle className="text-2xl">Bem vindo</CardTitle>
+                <CardDescription>
+                  Insira suas credenciais para acessar o painel de controle
+                </CardDescription>
+              </CardHeader>
 
-          <CardContent className="mb-2">
-            <form>
-              <div className="flex flex-col gap-10 p-1.5 ">
-                <div className="flex flex-col gap-2">
-                  <Label htmlFor="name">Name:</Label>
-                  <Input
-                    type="text"
-                    placeholder="Enter your name"
-                    name="name"
-                    id="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </div>
+              <CardContent className="mb-2">
+                <form>
+                  <div className="flex flex-col gap-10 p-1.5 ">
+                    <div className="flex flex-col gap-2">
+                      <Label htmlFor="name">Usuario:</Label>
+                      <InputGroup className="py-5">
+                        <InputGroupInput
+                          type="text"
+                          placeholder="Insira seu nome de usuario"
+                          name="name"
+                          id="name"
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                        />
 
-                <div className="flex flex-col gap-2">
-                  <Label htmlFor="password">Password:</Label>
-                  <InputGroup>
-                    <InputGroupInput
-                      type={ isVisible ? "text" : "password"}
-                      placeholder="Enter your password"
-                      name="password"
-                      id="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <InputGroupAddon align="inline-end">
-                      <InputGroupButton onClick={handleShowPassword}>
-                        {isVisible ? <EyeIcon /> : <EyeOffIcon />}
-                      </InputGroupButton>
-                    </InputGroupAddon>
-                  </InputGroup>
-                </div>
-              </div>
-            </form>
-          </CardContent>
+                        <InputGroupAddon align="inline-end">
+                          <Separator
+                            orientation="vertical"
+                            className="!h-5 !w-0.5"
+                          />
+                          <User className="size-5" strokeWidth={2.5} />
+                        </InputGroupAddon>
+                      </InputGroup>
+                    </div>
 
-          <CardFooter className="flex-col gap-2">
-            <Button type="submit">Login</Button>
-          </CardFooter>
-        </Card>
+                    <div className="flex flex-col gap-2">
+                      <Label htmlFor="password">Senha:</Label>
+                      <InputGroup className="py-5">
+                        <InputGroupInput
+                          type={isVisible ? "text" : "password"}
+                          placeholder="Insira sua senha"
+                          name="password"
+                          id="password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <Separator
+                          orientation="vertical"
+                          className="!h-5 !w-0.5"
+                        />
+                        <InputGroupAddon align="inline-end">
+                          <InputGroupButton
+                            onClick={handleShowPassword}
+                            size="icon-sm"
+                          >
+                            {isVisible ? (
+                              <EyeIcon className="size-5" strokeWidth={2.5} />
+                            ) : (
+                              <EyeOffIcon
+                                className="size-5"
+                                strokeWidth={2.5}
+                              />
+                            )}
+                          </InputGroupButton>
+                        </InputGroupAddon>
+                      </InputGroup>
+                    </div>
+                  </div>
+                </form>
+              </CardContent>
+
+              <CardFooter className="flex-col mt-4">
+                <Button type="submit" className="w-full">
+                  Entra
+                </Button>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+        <div className="min-h-svh bg-primary-foreground bg-[url('./assets/background.png')] bg-cover bg-no-repeat bg-center bg-blend-multiply flex flex-col items-end justify-end py-10 px-4">
+          <h1 className="text-white text-3xl text-end mb-10">"Otimizando o consumo industrial através de dados precisos e monitoramento"</h1>
+          <h4 className="text-gray-300 text-sm">Painel de Controle</h4>
+        </div>
       </div>
     </>
   )
