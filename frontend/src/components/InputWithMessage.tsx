9a77ee633f02e0ed942f1ad4295f9981706ad398
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-01-15 12:31:05 -0300

    feat(components): add spinner component and personalizate Input components

---
+++ frontend/src/components/InputWithMessage.tsx
@@ -0,0 +1,43 @@
+import { Label } from "@/components/ui/label"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group"
+import type { ChangeEvent, ReactNode } from "react"
+
+type Props = {
+  label: string
+  name: string
+  value: string
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+  addons?: ReactNode
+}
+
+export const InputWithMessage = ({
+  label,
+  name,
+  value,
+  onChange,
+  addons,
+}: Props) => {
+  return (
+    <>
+      <div className="flex flex-col gap-2">
+        <Label htmlFor={name}>{label}</Label>
+        <InputGroup className="py-5">
+          <InputGroupInput
+            type="text"
+            placeholder="Insira seu nome de usuario"
+            name={name}
+            id={name}
+            value={value}
+            onChange={onChange}
+          />
+
+          <InputGroupAddon align="inline-end">{addons}</InputGroupAddon>
+        </InputGroup>
+      </div>
+    </>
+  )
+}
