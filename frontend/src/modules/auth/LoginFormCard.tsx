4840c68232034078914ff60ec617da14ecc7ae10
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-01-15 12:31:32 -0300

    feat(components): add new components and logic error in forms

--- frontend/src/modules/auth/LoginFormCard.tsx
+++ frontend/src/modules/auth/LoginFormCard.tsx
@@ -6,48 +6,45 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import {
-  InputGroup,
-  InputGroupAddon,
-  InputGroupButton,
-  InputGroupInput,
-} from "@/components/ui/input-group"
-import { Label } from "@/components/ui/label"
 import { Separator } from "@/components/ui/separator"
 import { useEffect, useState, type FormEvent } from "react"
-import { EyeIcon, EyeOffIcon, User } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { useAuth } from "./hooks/auth.hook"
 import { useNavigate } from "react-router"
+import { InputPasswordWithMessage } from "@/components/InputPasswordWithMessage"
+import { User } from "lucide-react"
+import { Spinner } from "@/components/ui/spinner"
+import { InputWithMessage } from "@/components/InputWithMessage"
 
 export const LoginFormCard = () => {
   const [name, setName] = useState("")
   const [password, setPassword] = useState("")
-  const [isVisible, setIsVisible] = useState(false)
-  const { token, login } = useAuth()
+  const [isError, setIsError] = useState(false)
+
+  const { token, login, error, isLoading } = useAuth()
   const navigation = useNavigate()
 
-  const handleShowPassword = () => {
-    setIsVisible(!isVisible)
-  }
-
   useEffect(() => {
-    if (token === undefined) return;
-    
+    if (error) setIsError(error)
+
     if (!token) {
-      console.error("FAILED LOGIN")
       return
     }
 
     console.info("LOGIN IS SUCCESS")
     navigation("/", { replace: true })
-  }, [token])
+  }, [token, error])
+
+  useEffect(() => {
+    setIsError(false)
+  }, [name, password])
 
   const handleLoginForm = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     if (name.length <= 0 || password.length <= 0) {
       console.error("NAME OR PASSWORD INVALID")
+      setIsError(true)
       return
     }
 
@@ -67,58 +64,30 @@ export const LoginFormCard = () => {
         <CardContent className="mb-2">
           <form id="login-form" onSubmit={handleLoginForm}>
             <div className="flex flex-col gap-10 p-1.5 ">
-              <div className="flex flex-col gap-2">
-                <Label htmlFor="name">Usuario:</Label>
-                <InputGroup className="py-5">
-                  <InputGroupInput
-                    type="text"
-                    placeholder="Insira seu nome de usuario"
-                    name="name"
-                    id="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-
-                  <InputGroupAddon align="inline-end">
+              <InputWithMessage
+                label="Usuario:"
+                name="name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                addons={
+                  <>
                     <Separator orientation="vertical" className="h-5! w-0.5!" />
                     <User className="size-5" strokeWidth={2.5} />
-                  </InputGroupAddon>
-                </InputGroup>
-              </div>
-
-              <div className="flex flex-col gap-2">
-                <Label htmlFor="password">Senha:</Label>
-                <InputGroup className="py-5">
-                  <InputGroupInput
-                    type={isVisible ? "text" : "password"}
-                    placeholder="Insira sua senha"
-                    name="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <Separator orientation="vertical" className="h-5! w-0.5!" />
-                  <InputGroupAddon align="inline-end">
-                    <InputGroupButton
-                      onClick={handleShowPassword}
-                      size="icon-sm"
-                    >
-                      {isVisible ? (
-                        <EyeIcon className="size-5" strokeWidth={2.5} />
-                      ) : (
-                        <EyeOffIcon className="size-5" strokeWidth={2.5} />
-                      )}
-                    </InputGroupButton>
-                  </InputGroupAddon>
-                </InputGroup>
-              </div>
+                  </>
+                }
+              />
+              <InputPasswordWithMessage
+                value={password}
+                setValue={setPassword}
+                isError={isError}
+              />
             </div>
           </form>
         </CardContent>
 
-        <CardFooter className="flex-col mt-4">
+        <CardFooter className="flex-col mt-4 px-44">
           <Button type="submit" form="login-form" className="w-full">
-            Entra
+            {isLoading ? <Spinner className="size-6" /> : "Entrar"}
           </Button>
         </CardFooter>
       </Card>
