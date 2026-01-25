b73e33a9939f0dc7e478134d58973a94ce788d95
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-01-25 16:36:42 -0300

    feat(dashboard): add month grouping and factory-year filtering

--- backend/src/modules/dashboard/dashboard.controller.ts
+++ backend/src/modules/dashboard/dashboard.controller.ts
@@ -6,27 +6,49 @@ import { Roles } from 'src/common/decorators/roles.decorator';
 import { Role } from 'src/common/enums/role.enum';
 import { PlatformSelect } from 'src/common/decorators/platform.decorator';
 import { Platform } from 'src/common/enums/platform.enum';
+import { FactoriesService } from '../factories/factories.service';
 
 @Controller('dashboard')
 export class DashboardController {
-  constructor(private readonly DashboardService: DashboardService) {}
+  constructor(
+    private readonly dashboardService: DashboardService,
+    private readonly factoriesService: FactoriesService,
+  ) {}
 
   @Get('consumption')
   @Roles(Role.Admin, Role.Manager)
   @PlatformSelect(Platform.Web)
   async consupmtionDashboard(
     @Query('groupBy', new ZodValidationPipe(GroupsSchema)) group: GroupsDto,
+    @Query('factoryId') factoryId?: string,
+    @Query('year') year?: string,
   ) {
+    const factoryIdNum = factoryId ? Number(factoryId) : undefined;
+    const yearNum = year ? Number(year) : undefined;
     switch (group) {
       case 'machine':
-        return { data: await this.DashboardService.getKwhByMachines() };
+        return { data: await this.dashboardService.getKwhByMachinesAll() };
 
       case 'factory':
-        return { data: await this.DashboardService.getKwhByFactories() };
+        return { data: await this.dashboardService.getKwhByFactoriesAll() };
 
       case 'month':
+        if (factoryIdNum) {
+          const factory =
+            await this.factoriesService.getFactoryInfo(factoryIdNum);
+          return {
+            id: factory.id,
+            name: factory.name,
+            data: await this.dashboardService.getKwhMonthByFactory(
+              factoryIdNum,
+              yearNum,
+            ),
+          };
+        }
+
+        break;
       default:
-        return { data: await this.DashboardService.getKwhByMonth() };
+        return { data: await this.dashboardService.getKwhByMonth() };
     }
   }
 }
--- backend/src/modules/dashboard/dashboard.module.ts
+++ backend/src/modules/dashboard/dashboard.module.ts
@@ -4,9 +4,10 @@ import { DashboardService } from './dashboard.service';
 import { ConsumptionRepository } from './repositories/consumption.repository';
 import { Registries } from 'src/modules/registries/entities/registries.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FactoriesModule } from '../factories/factories.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Registries])],
+  imports: [TypeOrmModule.forFeature([Registries]), FactoriesModule],
   controllers: [DashboardController],
   providers: [DashboardService, ConsumptionRepository],
 })
