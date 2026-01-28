fafaaa434c5e36e7ff274d464d9eb9975b6c09ee
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-01-28 07:37:17 -0300

    fix(registries): adjusment schema registries in frontend and add .gitignore .env.dev

--- frontend/src/modules/machines/schemas/registry.schema.ts
+++ frontend/src/modules/machines/schemas/registry.schema.ts
@@ -4,7 +4,8 @@ export const registrySchema = z.object({
   value: z
     .number({ message: "Valor deve ser um número" })
     .nonnegative("Valor deve ser positivo ou zero")
-    .min(0.01, "Valor deve ser maior que zero"),
+    .min(0.01, "Valor deve ser maior que zero")
+    .max(10000, "Valor deve ser menor que 10000"),
 })
 
 export type RegistryFormData = z.infer<typeof registrySchema>
